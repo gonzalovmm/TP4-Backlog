c97d9fd4053605b77002b904a541922c98675706
gonzalovmm <gonzavm00@gmail.com>
2025-09-19 13:11:49 -0300

    prueba 2

--- src/main/java/com/example/demo/Entities/Usuario.java
+++ src/main/java/com/example/demo/Entities/Usuario.java
@@ -1,6 +1,5 @@
 package com.example.demo.Entities;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,22 +8,26 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Usuario {
-    private String nombre;
-    private String email;
-    private String contraseña;
-    private boolean esPremium;
+  private String nombre;
+  private String email;
+  private String contraseña;
+  private boolean esPremium;
 
-    //Métodos
-    public boolean puedeSimular() {return esPremium;}
-    public boolean emailValido() {return email != null && email.contains("@");}
-    public boolean nombreValido(Usuario usuario){
-        String nombre = usuario.getNombre();
-        if (nombre == null || nombre.isEmpty()) {
-            return false;
-        }
-        char primeraLetra = nombre.charAt(0);
-        return Character.isUpperCase(primeraLetra);
+  // Métodos
+  public boolean puedeSimular() {
+    return esPremium;
+  }
+
+  public boolean emailValido() {
+    return email != null && email.contains("@");
+  }
+
+  public boolean nombreValido(Usuario usuario) {
+    String nombre = usuario.getNombre();
+    if (nombre == null || nombre.isEmpty()) {
+      return false;
     }
+    char primeraLetra = nombre.charAt(0);
+    return Character.isUpperCase(primeraLetra);
+  }
 }
-
-
